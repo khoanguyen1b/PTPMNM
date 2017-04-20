9981ba0102f686ca04fdd826028cecc92657eefb
Phucbui <phucbui1b@gmail.com>
2017-04-20 13:13:26 +0700

    Code Shell Script

---
+++ Thuchanh/Maxab.sh
@@ -0,0 +1,8 @@
+m=$1
+if test $m -lt $2
+then
+m=$2
+else 
+m=$1
+fi
+echo "$m la so lon nhat"
---
+++ Thuchanh/Maxabc.sh
@@ -0,0 +1,10 @@
+m=$1
+if test $m -lt $2
+then
+m=$2
+fi
+if test $m -lt $3
+then
+m=$3
+fi
+echo "$m la so lon nhat trong 3 so $1 $2 $3"
